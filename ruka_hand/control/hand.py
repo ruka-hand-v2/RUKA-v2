c4bfe88931dcad90873dd6794f93ca0441c41ada
pianapolataa <sissi.hu@hotmail.com>
2026-03-30 15:12:26 -0400

    add calibration code

--- ruka_hand/control/hand.py
+++ ruka_hand/control/hand.py
@@ -53,15 +53,23 @@ class Hand:
         # self.operating_mode = 3  # 5: current-based position control
 
         repo_root = get_repo_root()
-        if hand_type == "right":
-            self.curled_bound = np.array([2609, 2852, 1375, 1811, 2605, 2026, 1511, 1187, 354, 2184, 1969, 506, 2241, 1795, 1617, 1850])
-            self.tensioned_pos = np.array([3500,3283, 2511, 1550, 3461, 897, 2590, 1371, 1047, 1400, 962, 930, 2980, 2340, 2417, 1400])
-        elif hand_type == "left":
-            self.curled_bound = np.array([1572, 2919, 1616, 3013, 3157, 2340, 1034, 1757, 3502, 1159, 511, 1952, 2170, 3380, 3204, 1338])
-            self.tensioned_pos = np.array([1040, 2581, 660, 3947, 2973, 2814, 746, 1070, 3364, 1582, 1502, 1800, 1864, 2496, 2601, 1927])
-        elif hand_type == "v1_right":
-            self.curled_bound = np.array([2416, 3327, 2435, 986, 1248, 2939, 1950, 2808, 2077, 1893, 2230])
-            self.tensioned_pos = np.array([3225, 3794, 2682, 1818, 1932, 3815, 2608, 3390, 3059, 2708, 3208])
+        curled_path = os.path.join(repo_root, "curl_limits", f"{hand_type}_curl_limits.npy")
+        tension_path = os.path.join(repo_root, "curl_limits", f"{hand_type}_tension_limits.npy")
+        print(curled_path)
+        if os.path.exists(curled_path) and os.path.exists(tension_path):
+            print(f"Loading saved calibration for {hand_type}...")
+            self.curled_bound = np.load(curled_path)
+            self.tensioned_pos = np.load(tension_path)
+        else:
+            if hand_type == "right":
+                self.curled_bound = np.array([2609, 2852, 1375, 1811, 2605, 2026, 1511, 1187, 354, 2184, 1969, 506, 2241, 1795, 1617, 1850])
+                self.tensioned_pos = np.array([3500,3283, 2511, 1550, 3461, 897, 2590, 1371, 1047, 1400, 962, 930, 2980, 2340, 2417, 1400])
+            elif hand_type == "left":
+                self.curled_bound = np.array([1572, 2919, 1616, 3013, 3157, 2340, 1034, 1757, 3502, 1159, 511, 1952, 2170, 3380, 3204, 1338])
+                self.tensioned_pos = np.array([1040, 2581, 660, 3947, 2973, 2814, 746, 1070, 3364, 1582, 1502, 1800, 1864, 2496, 2601, 1927])
+            elif hand_type == "v1_right":
+                self.curled_bound = np.array([2416, 3327, 2435, 986, 1248, 2939, 1950, 2808, 2077, 1893, 2230])
+                self.tensioned_pos = np.array([3225, 3794, 2682, 1818, 1932, 3815, 2608, 3390, 3059, 2708, 3208])
 
         self.min_lim = np.minimum(self.tensioned_pos, self.curled_bound)
         self.max_lim = np.maximum(self.tensioned_pos, self.curled_bound) 
@@ -228,3 +236,6 @@ class Hand:
     def read_single_cur(self, motor_id):
         cur = self.dxl_client.read_single_cur(motor_id)
         return cur
+
+if __name__ == "__main__":
+    hand= Hand("left")
